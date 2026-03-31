d289d01352fe247a5b979b798c3dd887cb83fa82
agnidhis <nidhiagarwal@unomaha.edu>
2026-03-31 18:10:11 +0000

    ImageFilters

--- ImageFilters.py
+++ ImageFilters.py
@@ -1,10 +1,9 @@
 # Lab 9 – Image Processing
-# Name:
-# Date:
-# Assignment:
+# Name: Nidhi Agarwal
+# Date: 03/31/2026
+# Assignment: Change color
 
 from PIL import Image
-
 
 def swapGreenBlue(img):
     """Swap the green and blue values for every pixel in the image."""
@@ -13,6 +12,12 @@ def swapGreenBlue(img):
     width, height = img.size
 
     # TODO: Loop through every pixel and swap green and blue values
+    for x in range(width):
+        for y in range(height):
+            r, g, b = pixels[x, y][:3]   # get RGB values
+
+            # Swap green and blue
+            pixels[x, y] = (r, b, g)
 
     img.save("swapGB.png")
 
@@ -25,6 +30,16 @@ def darken(img, amount):
 
     # TODO: Loop through every pixel and reduce RGB values by amount
     # Make sure values do not go below 0
+    for x in range(width):
+        for y in range(height):
+            r, g, b = pixels[x, y][:3]
+
+            # Reduce brightness safely
+            r = max(0, r - amount)
+            g = max(0, g - amount)
+            b = max(0, b - amount)
+
+            pixels[x, y] = (r, g, b)
 
     img.save("darkImg.png")
 
@@ -49,11 +64,11 @@ def main():
     myImg = Image.open("durango.png")
 
     # Example (already completed)
-    # bwFilter(myImg)
+    bwFilter(myImg)
 
     # Uncomment each function as you complete it
-    # swapGreenBlue(myImg)
-    # darken(myImg, 20)
+    swapGreenBlue(myImg)
+    darken(myImg, 20)
 
 
 if __name__ == "__main__":
